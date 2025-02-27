37e845626ad9cdbe0b0da367f36033c2e8d62613
Rajlakshmi <122472826+rajlakshmi18704@users.noreply.github.com>
2025-02-27 15:01:23 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,19 +1,18 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBEDNojwx-RcdMDdqgEMEI8iVoVGDzJeaU",
-    authDomain: "cryptohunter-95375.firebaseapp.com",
-    projectId: "cryptohunter-95375",
-    storageBucket: "cryptohunter-95375.firebasestorage.app",
-    messagingSenderId: "949833742141",
-    appId: "1:949833742141:web:c3761534041d2c0b2b7754",
-    measurementId: "G-0V3JTDSSWW"
-  };
-  const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-  export default firebaseConfig;
+export const db = getFirestore(app);
+export default app;
